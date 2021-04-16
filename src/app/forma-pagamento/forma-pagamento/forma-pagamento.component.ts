119cd3df3b11b12a186219a7225a81d46837a229
TalissonMelo <talissonmelorodrigues@gmail.com>
2021-04-16 20:43:36 -0300

    Criado Deleção de FormaPagamento

--- src/app/forma-pagamento/forma-pagamento/forma-pagamento.component.ts
+++ src/app/forma-pagamento/forma-pagamento/forma-pagamento.component.ts
@@ -8,6 +8,8 @@ import { FormaPagamentoService } from 'src/app/service/formaPagamento';
 })
 export class FormaPagamentoComponent implements OnInit {
   public formaPagamentos: FormaPagamento[] = [];
+  public formaPagamento: FormaPagamento;
+  public erroDelcao: string;
 
   constructor(private service: FormaPagamentoService) { }
 
@@ -18,4 +20,18 @@ export class FormaPagamentoComponent implements OnInit {
     this.service.listar().subscribe(res => this.formaPagamentos = res);
   }
 
+  preparaDelecao(pagamento: FormaPagamento) {
+    this.formaPagamento = pagamento;
+  }
+
+  deletar(id: number): void {
+    this.service.deletar(id).subscribe(() => {
+      let index: number = this.formaPagamentos.findIndex((pagamento) => pagamento.id == id);
+      this.formaPagamentos.splice(index, 1);
+      this.erroDelcao = null;
+    }, error => {
+      this.erroDelcao = error.error.detalhe;
+    });
+
+  }
 }
