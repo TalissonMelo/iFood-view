d2affc283de6f68c5d1ac996b2f0b78a88659c73
TalissonMelo <talissonmelorodrigues@gmail.com>
2021-04-18 14:06:31 -0300

    Criando Tela de Restaurante

--- src/app/restaurante/restaurante.module.ts
+++ src/app/restaurante/restaurante.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RestauranteComponent } from './restaurante/restaurante.component';
 import { RestauranteFrmComponent } from './restaurante-frm/restaurante-frm.component';
 import { RestauranteRoutingModule } from './resturante-routing.module';
+import { FormsModule } from '@angular/forms';
+import { TemplateModule } from '../template/template.module';
 
 @NgModule({
-  declarations: [RestauranteComponent, RestauranteFrmComponent],
+  declarations: [
+    RestauranteComponent, 
+    RestauranteFrmComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
+    TemplateModule,
     RestauranteRoutingModule
   ]
 })
